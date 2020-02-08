6abc1672daebe3734375296c3efac307ab3a041b
AshwinSaxena <quizmasterashwin@gmail.com>
2020-02-09 01:22:15 +0530

    Cleanup

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -70,7 +70,7 @@ export const IndexPageTemplate = ({
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="content">
-                <div className="content">
+                {/* <div className="content">
                   <div className="tile">
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
@@ -93,7 +93,7 @@ export const IndexPageTemplate = ({
                       See all products
                     </Link>
                   </div>
-                </div>
+                </div> */}
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest stories
