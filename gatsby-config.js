2da5d30c897bfc119df0f47945370379f05f7fb7
Ashwin Saxena <quizmaster.ashwin@gmail.com>
2020-02-10 13:10:56 +0530

    Updated meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Blog.TalkingDev',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'TalkingDev Tech Blog. JavaScript, Angular, React, CSS3, Material, Bootstrap, Svelte, Vue and everything Frontend.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
