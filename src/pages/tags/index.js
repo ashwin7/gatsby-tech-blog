7384290f3670b767dd06f8fd1a535b95571b2f17
Ashwin Saxena <ashwinsaxena@PNQ-ASAXENA.local>
2020-06-19 18:18:02 +0530

    Fixed tagDesc bug

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -5,7 +5,12 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout'
 import Pricing from '../../components/Pricing'
 import Tag from '../../components/Tag'
-import DESC from './tagDesc'
+
+const DESC = {
+  'Programming': 'We are all apprentices in a craft where no one ever becomes a master. Read my little nuggets of programming knowledge I\'ve learnt over the years.',
+  'Books': 'It is books that are the key to the wide world; if you can’t do anything else, read all that you can. Here are few book recommendations from me.',
+  'Investing': 'My Ideas about investing'
+}
 
 const TagsPage = ({
   data: {
