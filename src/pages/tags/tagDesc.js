8c5d64be77c3e5c699685d6412e8e54cda6990a3
Ashwin Saxena <quizmaster.ashwin@gmail.com>
2020-06-19 13:41:40 +0530

    Updates Tags Desc

--- src/pages/tags/tagDesc.js
+++ src/pages/tags/tagDesc.js
@@ -1,6 +1,7 @@
 const DESC = {
     'Programming': 'We are all apprentices in a craft where no one ever becomes a master. Read my little nuggets of programming knowledge I\'ve learnt over the years.',
-    'Books': 'It is books that are the key to the wide world; if you can’t do anything else, read all that you can. Here are few book recommendations from me.'
+    'Books': 'It is books that are the key to the wide world; if you can’t do anything else, read all that you can. Here are few book recommendations from me.',
+    'Investing': 'My Ideas about investing'
 }
 
 export default DESC;
